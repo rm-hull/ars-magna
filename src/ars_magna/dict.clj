a7a45af5416e3828f430c2116fe6f92fce02e74a
Richard Hull <rm_hull@yahoo.co.uk>
2016-03-20 19:50:55 +0000

    More dict functions

--- src/ars_magna/dict.clj
+++ src/ars_magna/dict.clj
@@ -1,6 +1,8 @@
 (ns ars-magna.dict
   (:require
-    [clojure.string :refer [split-lines lower-case]]))
+    [clojure.set :refer [difference]]
+    [clojure.string :refer [split-lines lower-case]]
+    [ars-magna.letter-frequencies :refer [rarest-letter]]))
 
 (defn load-word-list [lang]
   (->>
@@ -15,3 +17,39 @@
       (update acc (count word) conj word))
     (sorted-map)
     dict))
+
+(defn words-of-size
+  ([index n] (words-of-size index n 0))
+  ([index n min-size]
+    (if (< n min-size)
+      nil
+      (lazy-cat
+        (index n)
+        (words-of-size index (dec n) min-size)))))
+
+(defn can-make? [set target]
+  (if (empty? target)
+    true
+    (let [m (first target)]
+      (if (contains? set m)
+        (can-make?
+          (difference set #{m})
+          (rest target))
+        false))))
+
+(defn find-in [index word min-size lang]
+  (let [rarest-letter (rarest-letter word lang)
+        all-letters (set word)
+        pred (fn [word]
+               (and
+                 (contains? (set word) rarest-letter)
+                 (can-make? all-letters word)))]
+    (->>
+      (words-of-size index (count word) min-size)
+      (filter pred))))
+
+(defn remaining-chars [word1 word2]
+  (difference
+    (set word1)
+    (set word2)))
+
--- test/ars_magna/dict_test.clj
+++ test/ars_magna/dict_test.clj
@@ -3,22 +3,49 @@
     [clojure.test :refer :all]
     [ars-magna.dict :refer :all]))
 
+(def test-dict
+  ["hello" "hat" "gloves" "time" "normally"
+   "at" "banana" "leaf" "lead" "and" "you"])
+
 (deftest check-load-word-list
   (is (= 99171 (count (load-word-list :en-GB)))))
 
 (deftest check-partitioning
-  (let [index (partition-by-word-length
-           ["hello" "hat" "gloves" "time" "normally"
-            "at" "banana" "leaf" "lead" "and" "you"])]
+  (let [index (partition-by-word-length test-dict)]
     (is (nil? (index 0)))
     (is (nil? (index 1)))
     (is (= (index 2) ["at"]))
     (is (= (index 3) ["you" "and" "hat"]))
     (is (= (index 4) ["lead" "leaf" "time"]))
-    (is (= (index 5) ["hello"]))2
+    (is (= (index 5) ["hello"]))
     (is (= (index 6) ["banana" "gloves"]))
     (is (nil? (index 7)))
     (is (= (index 8) ["normally"]))))
 
 
+(deftest check-words-of-size
+  (let [index (partition-by-word-length test-dict)]
+    (is (= ["normally" "banana" "gloves" "hello"] (words-of-size index 12 5)))
+    (is (= ["normally" "banana" "gloves" "hello" "lead"
+            "leaf" "time" "you" "and" "hat" "at"] (words-of-size index 12)))
+    (is (= ["lead" "leaf" "time" "you" "and" "hat" "at"] (words-of-size index 4)))
+    (is (= ["you" "and" "hat"] (words-of-size index 3 3)))
+    (is (= ["you" "and" "hat" "at"] (words-of-size index 3)))
+    (is (= ["at"] (words-of-size index 2)))
+    (is (empty? (words-of-size index 1)))
+    (is (empty? (words-of-size index 0)))))
 
+(deftest check-can-make?
+  (is (true? (can-make? (set "compute") "cute")))
+  (is (true? (can-make? (set "compute") "pout")))
+  (is (true? (can-make? (set "compute") "mute")))
+  (is (true? (can-make? (set "compute") "pot")))
+  (is (true? (can-make? (set "compute") "top")))
+  (is (false? (can-make? (set "compute") "poot"))))
+
+(deftest check-find-in
+  (let [index (partition-by-word-length test-dict)]
+    (is (= ["hat"] (find-in index "that" 0 :en-GB)))))
+
+(deftest check-remaining-chars
+  (is (= (set "mute") (remaining-chars "compute" "cop"))))
